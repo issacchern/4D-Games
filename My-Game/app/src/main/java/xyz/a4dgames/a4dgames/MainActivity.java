d975ad1a1dcd1d224ceed7b892c97c1093a8e82d
issacchern <issac.chua12@gmail.com>
2016-05-17 15:14:23 -0400

    initial commit

---
+++ My-Game/app/src/main/java/xyz/a4dgames/a4dgames/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.a4dgames.a4dgames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
